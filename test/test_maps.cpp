ba68fafa07027304629fc9013c56751cc4a66e16
vinch <a478honor0109@gmail.com>
2025-03-23 12:14:41 +0300

    bld fix

--- test/test_maps.cpp
+++ test/test_maps.cpp
@@ -14,9 +14,9 @@ TEST(UnorderedMapTest, can_insert_elem_with_uniq_key)
 	M.insert(4, 7);
 	M.insert(7, 12);
 	M.insert(9, 1);
-	EXPECT_EQ(M[0], std::make_pair(4, 7));
-	EXPECT_EQ(M[1], std::make_pair(7, 12));
-	EXPECT_EQ(M[2], std::make_pair(9, 1));
+	EXPECT_EQ(M[0], std::make_pair(static_cast<size_t>(4), 7));
+	EXPECT_EQ(M[1], std::make_pair(static_cast<size_t>(7), 12));
+	EXPECT_EQ(M[2], std::make_pair(static_cast<size_t>(9), 1));
 }
 TEST(UnorderedMapTest, insertion_change_size_correctly)
 {
@@ -54,38 +54,38 @@ TYPED_TEST_CASE(TUnorderedMapTest, types);
 // typed tests for UndorderedTable
 TYPED_TEST(TUnorderedMapTest, cant_insert_elem_with_non_uniq_key)
 {
-	EXPECT_FALSE(p->insert(4, 12));
+	EXPECT_FALSE(this->p->insert(4, 12));
 }
 TYPED_TEST(TUnorderedMapTest, can_find_existing_elem)
 {
-	EXPECT_EQ((p->find(7)).value(), std::make_pair(7, 12));
+	EXPECT_EQ((this->p->find(7)).value(), std::make_pair(static_cast<size_t>(7), 12));
 }
 TYPED_TEST(TUnorderedMapTest, cant_find_non_existing_elem)
 {
-	EXPECT_FALSE((p->find(2)).has_value());
+	EXPECT_FALSE((this->p->find(2)).has_value());
 }
 TYPED_TEST(TUnorderedMapTest, can_find_idx_of_existing_elem)
 {
-	EXPECT_EQ((p->find_idx(7)).value(), 1);
+	EXPECT_EQ((this->p->find_idx(7)).value(), 1);
 }
 TYPED_TEST(TUnorderedMapTest, cant_find_idx_of_non_existing_elem)
 {
-	EXPECT_FALSE((p->find(2)).has_value());
+	EXPECT_FALSE((this->p->find(2)).has_value());
 }
 TYPED_TEST(TUnorderedMapTest, can_erase_existing_elem)
 {
-	p->erase(7);
-	EXPECT_FALSE(p->find(7).has_value());
+	this->p->erase(7);
+	EXPECT_FALSE(this->p->find(7).has_value());
 }
 TYPED_TEST(TUnorderedMapTest, cant_erase_non_existing_elem)
 {
-	EXPECT_FALSE(p->erase(10));
+	EXPECT_FALSE(this->p->erase(10));
 }
 TYPED_TEST(TUnorderedMapTest, erasing_change_size_correctly)
 {
-	p->erase(4);
-	p->erase(7);
-	EXPECT_EQ(p->getSize(), 1);
+	this->p->erase(4);
+	this->p->erase(7);
+	EXPECT_EQ(this->p->getSize(), 1);
 }
 
 // general tests for OrderedTable
@@ -149,34 +149,34 @@ TYPED_TEST_CASE(TOrderedMapTest, types);
 // typed tests for UndorderedTable
 TYPED_TEST(TOrderedMapTest, cant_insert_elem_with_non_uniq_key)
 {
-	EXPECT_FALSE(p->insert(4, 12));
+	EXPECT_FALSE(this->p->insert(4, 12));
 }
 TYPED_TEST(TOrderedMapTest, can_find_existing_elem)
 {
-	EXPECT_EQ((p->find(7)).value(), std::make_pair(7, 12));
+	EXPECT_EQ((this->p->find(7)).value(), std::make_pair(static_cast<size_t>(7), 12));
 }
 TYPED_TEST(TOrderedMapTest, cant_find_non_existing_elem)
 {
-	EXPECT_FALSE((p->find(2)).has_value());
+	EXPECT_FALSE((this->p->find(2)).has_value());
 }
 TYPED_TEST(TOrderedMapTest, can_erase_existing_elem)
 {
-	p->erase(7);
-	EXPECT_FALSE(p->find(7).has_value());
+	this->p->erase(7);
+	EXPECT_FALSE(this->p->find(7).has_value());
 }
 TYPED_TEST(TOrderedMapTest, cant_erase_non_existing_elem)
 {
-	EXPECT_FALSE(p->erase(10));
+	EXPECT_FALSE(this->p->erase(10));
 }
 TYPED_TEST(TOrderedMapTest, erasing_change_size_correctly)
 {
-	p->erase(4);
-	p->erase(7);
-	EXPECT_EQ(p->getSize(), 1);
+	this->p->erase(4);
+	this->p->erase(7);
+	EXPECT_EQ(this->p->getSize(), 1);
 }
 TYPED_TEST(TOrderedMapTest, erasing_save_correct_order)
 {
-	p->erase(7);
-	EXPECT_EQ((*p)[0], std::make_pair(4, 7));
-	EXPECT_EQ((*p)[1], std::make_pair(9, 1));
+	this->p->erase(7);
+	EXPECT_EQ((*(this->p))[0], std::make_pair(static_cast<size_t>(4), 7));
+	EXPECT_EQ((*(this->p))[1], std::make_pair(static_cast<size_t>(9), 1));
 }
